3bb5a0113c275d7b998aa5df571dd1c1697a4ea3
Zé Pedro Matos <revolty@gmail.com>
2019-08-15 17:12:50 +0100

    Error Handling: Duplicate Usernames

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,13 +1,20 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username'])
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    username: string;
+  @Column()
+  username: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 }
